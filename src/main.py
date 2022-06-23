5e2d0fa0c569ed112b1c7205334d416396c95344
Jose <josejesuscedeno@gmail.com>
2022-06-23 10:10:29 +0000

    creados los modelos y la bd, falta los endpoints

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, FavoritesPeople, FavoritesPlanets
 #from models import Person
 
 app = Flask(__name__)
@@ -32,6 +32,15 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
